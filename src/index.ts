ff5c59d1c4d5db69a77c1c83242a95c554f36d9a
Will Nelson <nelson.will@live.com>
2018-03-18 17:15:13 -0700

    actually export things

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,18 @@ import Guild from './Guild';
 import GuildMember from './GuildMember';
 import Role from './Role';
 import User from './User';
+
+export * from './Channel';
+export * from './Emoji';
+export * from './Guild';
+export * from './GuildMember';
+export * from './Role';
+export * from './User';
+export {
+  Channel,
+  Emoji,
+  Guild,
+  GuildMember,
+  Role,
+  User,
+}
