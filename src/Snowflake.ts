542ec74ca7bcf63f912d5b62c2847b26fbb8d1be
Will Nelson <will@pleb.xyz>
2019-09-23 23:19:39 -0700

    ref(snowflake): add setters, default 0n

--- src/Snowflake.ts
+++ src/Snowflake.ts
@@ -1,22 +1,11 @@
 export default class Snowflake {
 	public static EPOCH = 1420070400000;
-	public static WORKER_ID = 0;
-	public static INCREMENT = 0;
 
 	protected bin: bigint;
 
-	constructor(bin?: bigint | string) {
-		if (typeof bin === 'bigint') {
-			this.bin = bin;
-		} else if (typeof bin === 'string') {
-			this.bin = BigInt(bin);
-		} else {
-			if (Snowflake.INCREMENT > 0xFFF) Snowflake.INCREMENT = 0;
-			this.bin = BigInt(Snowflake.INCREMENT++)
-				| BigInt(process.pid << 12)
-				| BigInt(Snowflake.WORKER_ID << 17)
-				| (BigInt(Date.now() - Snowflake.EPOCH) << 22n);
-		}
+	constructor(bin: bigint | string = 0n) {
+		if (typeof bin === 'string') this.bin = BigInt(bin);
+		else this.bin = bin;
 	}
 
 	public get createdAt(): Date {
@@ -25,16 +14,32 @@ export default class Snowflake {
 		return d;
 	}
 
+	public set createdAt(d: Date) {
+		this.bin |= BigInt(d.valueOf() - Snowflake.EPOCH) << 22n;
+	}
+
 	public get workerID(): number {
 		return Number((this.bin & 0x3E0000n) >> 17n);
+	}
+
+	public set workerID(id: number) {
+		this.bin |= BigInt(id << 17);
 	}
 
 	public get processID(): number {
 		return Number((this.bin & 0x1F000n) >> 12n);
 	}
 
+	public set processID(id: number) {
+		this.bin |= BigInt(id << 12);
+	}
+
 	public get increment(): number {
 		return Number(this.bin & 0xFFFn);
+	}
+
+	public set increment(incr: number) {
+		this.bin |= BigInt(incr);
 	}
 
 	public valueOf(): bigint {
