be1eb3f074cabd17a085e7b46f5533e872b28d17
Will Nelson <will@pleb.xyz>
2019-09-23 23:07:25 -0700

    fix: remove erlpack encoding

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-let erlpack: { pack: (d: any) => Buffer, unpack: (d: Buffer | Uint8Array) => any } | undefined;
-if (typeof window === 'undefined') {
-  try {
-    erlpack = require('erlpack');
-  } catch {}
-}
-
 import * as Errors from './errors';
 import Permissions from './Permissions';
 
@@ -13,21 +6,14 @@ export {
   Permissions,
 };
 
-export const encoding = erlpack ? 'etf' : 'json';
-
 export function encode(data: any): Buffer {
   if (Buffer.isBuffer(data)) return data;
-  return erlpack ? erlpack.pack(data) : Buffer.from(JSON.stringify(data));
+  return Buffer.from(JSON.stringify(data));
 }
 
 export function decode<T = any>(data: ArrayBuffer | string | Buffer[] | Buffer | Uint8Array): T {
   if (data instanceof ArrayBuffer) data = Buffer.from(data);
   else if (Array.isArray(data)) data = Buffer.concat(data);
-
-  if (erlpack) {
-    if (typeof data === 'string') data = Buffer.from(data);
-    return erlpack.unpack(data as Uint8Array | Buffer); // TS doesn't type guard "data" as not being an instance of ArrayBuffer
-  }
 
   if (Buffer.isBuffer(data)) data = data.toString();
   else if (typeof data !== 'string') data = Buffer.from(data).toString();
