c9c9c5534fa7dff2547c7c1a4b4931a0b9576659
Will Nelson <will@pleb.xyz>
2019-09-25 20:04:19 -0700

    feat: workflows (#1)
    * feat: workflows
    
    * chore(actions): add test job name
    
    * chore: upgrade deps, add babel class props
    
    * fix(build): always build against node 12
    
    * fix(actions): class properties on node 10, 11

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = {
   presets: [
     ['@babel/preset-env', {targets: {node: 'current'}}],
      '@babel/preset-typescript',
-	],
-	plugins: ['@babel/plugin-syntax-bigint']
+  ],
+  plugins: [
+    '@babel/plugin-syntax-bigint',
+    ['@babel/plugin-proposal-class-properties', { 'loose': true }]
+  ]
 };
