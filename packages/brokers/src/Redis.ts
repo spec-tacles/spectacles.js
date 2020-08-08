905ab5322002c936900d67774e41de571302823d
Will Nelson <will@wnelson.xyz>
2020-08-08 14:06:56 -0700

    Setup pnpm workspace

---
+++ packages/brokers/src/Redis.ts
@@ -0,0 +1,161 @@
+import { randomBytes } from 'crypto';
+import { readFileSync } from 'fs';
+import { zipObject, partition, isObject } from 'lodash';
+import { resolve } from 'path';
+import { encode } from '@spectacles/util';
+import Redis = require('ioredis');
+import Broker, { ResponseOptions } from './Base';
+
+declare module 'ioredis' {
+  interface Redis {
+    xcleangroup(key: string, group: string): Promise<boolean>;
+    xadd(key: Redis.KeyType, id: string, ...args: any[]): Promise<any>;
+  }
+}
+
+export interface PublishOptions {
+  timeout?: number;
+}
+
+export interface ClientOptions {
+  blockInterval?: number;
+  maxChunk?: number;
+  name?: string;
+}
+
+export interface RedisResponseOptions extends ResponseOptions {
+  ack: () => void;
+}
+
+Redis.Command.setArgumentTransformer('xadd', (args) => {
+  if (args.length === 3) {
+    const toAdd = args.pop();
+    if (!isObject(toAdd)) throw new Error('unable to add publish non-object');
+
+    let entries: Iterable<[any, any]>;
+    if (toAdd instanceof Map) entries = toAdd.entries();
+    else entries = Object.entries(toAdd);
+
+    const arr = [];
+    for (const [k, v] of entries) arr.push(k, v);
+    return args.concat(arr);
+  }
+
+  return args;
+});
+
+export default class RedisBroker<Send = any, Receive = any> extends Broker<Send, Receive, RedisResponseOptions> {
+  public name: string;
+  public blockInterval: number;
+  public maxChunk: number;
+
+  protected _listening: boolean = false;
+  protected _streamReadClient: Redis.Redis;
+  protected _rpcReadClient: Redis.Redis;
+
+  constructor(public group: string, public redis: Redis.Redis, options: ClientOptions = {}) {
+    super();
+
+    this.name = options.name || randomBytes(20).toString('hex');
+    this.blockInterval = options.blockInterval || 5000;
+    this.maxChunk = options.maxChunk || 10;
+
+    redis.defineCommand('xcleangroup', {
+      numberOfKeys: 1,
+      lua: readFileSync(resolve(__dirname, '..', '..', 'scripts', 'xcleangroup.lua')).toString(),
+    });
+
+    this._rpcReadClient = redis.duplicate();
+    this._rpcReadClient.on('messageBuffer', (channel: Buffer, message: Buffer) => {
+      const [, id] = channel.toString().split(':');
+      if (id) this._handleReply(id, message);
+    });
+
+    this._streamReadClient = redis.duplicate();
+  }
+
+  public publish(event: string, data: Send): Promise<string> {
+    return this.redis.xadd(event, '*', data);
+  }
+
+  public async call(method: string, data: Send, options: PublishOptions = {}): Promise<Receive> {
+    const id = await this.publish(method, data);
+    const rpcChannel = `${method}:${id}`;
+    await this._rpcReadClient.subscribe(rpcChannel);
+
+    try {
+      return await this._awaitResponse(id, options.timeout);
+    } finally {
+      this._rpcReadClient.unsubscribe(rpcChannel);
+    }
+  }
+
+  protected async _subscribe(events: string[]): Promise<void> {
+    await Promise.all(events.map(async event => {
+      try {
+        await this.redis.xgroup('CREATE', event, this.group, 0, 'MKSTREAM');
+      } catch (e) {
+        if (!(e instanceof (Redis as any).ReplyError)) throw e;
+      }
+    }));
+
+    this._listen();
+  }
+
+  protected async _unsubscribe(events: string[]): Promise<void> {
+    const cmds: string[][] = Array(events.length * 2);
+    for (let i = 0; i < cmds.length; i += 2) {
+      const event = events[i / 2];
+      cmds[i] = ['xgroup', 'delconsumer', event, this.group, this.name];
+      cmds[i+1] = ['xcleangroup', event, this.group];
+    }
+
+    await this.redis.pipeline(cmds).exec();
+  }
+
+  private async _listen(): Promise<void> {
+    if (this._listening) return;
+    this._listening = true;
+
+    let events: Array<string>;
+    while (true) {
+      events = [...this._subscribedEvents];
+
+      try {
+        let data = await this._streamReadClient.xreadgroup(
+          'GROUP', this.group, this.name,
+          'COUNT', String(this.maxChunk),
+          'BLOCK', String(this.blockInterval),
+          'STREAMS', ...events,
+          ...Array(events.length).fill('>'),
+        );
+
+        /*
+        data = {
+          event: {
+            id: packet,
+          },
+        }
+        */
+        if (!data) continue;
+
+        for (const [event, info] of data) {
+          for (const [id, packet] of info) {
+            let i = 0;
+            const obj = zipObject(...partition(packet, () => i++ % 2 === 0)) as unknown as Receive;
+
+            this._handleMessage(event, obj, {
+              reply: (data) => this.redis.publish(`${event}:${id}`, encode(data) as any),
+              ack: () => this.redis.xack(event, this.group, id),
+            });
+          }
+        }
+      } catch (e) {
+        this.emit('error', e);
+        break;
+      }
+    }
+
+    this._listening = false;
+  }
+}
---
+++ packages/rest/src/structures/Rest.ts
@@ -0,0 +1,173 @@
+import * as https from 'https';
+import { EventEmitter } from 'events';
+import { Headers, HeadersInit, RequestInit } from 'node-fetch';
+import { Readable } from 'stream';
+import LocalMutex from '../mutexes/Local';
+import RatelimitMutex from '../mutexes/RatelimitMutex';
+import Request from '../types/Request';
+import Bucket from './Bucket';
+import * as FormData from 'form-data';
+
+const { version } = require('../../package.json');
+
+export enum TokenType {
+	BOT = 'Bot',
+	BEARER = 'Bearer',
+	BASIC = 'Basic',
+}
+
+export interface Options {
+	token?: string;
+	tokenType: TokenType,
+	base: string,
+	version: number,
+	agent: https.Agent,
+	ua: string,
+	mutex: RatelimitMutex,
+	retryLimit: number,
+}
+
+export default class Rest extends EventEmitter {
+	public static setHeaders(req: RequestInit, headers: HeadersInit) {
+		if (Array.isArray(headers)) for (const [header, value] of headers) Rest.setHeader(req, header, value);
+		else if (headers instanceof Headers) for (const [header, value] of { [Symbol.iterator]: headers.entries }) Rest.setHeader(req, header, value);
+		else for (const [header, value] of Object.entries(headers)) Rest.setHeader(req, header, value);
+	}
+
+	public static setHeader(req: RequestInit, header: string, value: string) {
+		if (Array.isArray(req.headers)) req.headers.push([header, value]);
+		else if (req.headers instanceof Headers) req.headers.set(header, value);
+		else if (req.headers) req.headers[header] = value;
+		else req.headers = new Headers({ [header]: value });
+	}
+
+	public static hasHeader(req: RequestInit, header: string) {
+		if (Array.isArray(req.headers)) return req.headers.some(([name]) => name === header);
+		else if (req.headers instanceof Headers) return req.headers.has(header);
+		else if (req.headers) return Object.keys(req.headers).includes(header);
+
+		return false;
+	}
+
+	public options: Options;
+	public buckets: Map<string, Bucket> = new Map();
+
+	constructor(token: string, options?: Partial<Options>)
+	constructor(options?: Partial<Options>)
+	constructor(token: string | Partial<Options> = {}, options: Partial<Options> = {}) {
+		super();
+
+		if (typeof token === 'string') {
+			options.token = token;
+		} else {
+			options = token;
+		}
+
+		this.options = {
+			token: options.token,
+			tokenType: options.tokenType ?? TokenType.BOT,
+			base: options.base ?? 'https://discordapp.com',
+			version: options.version ?? 6,
+			agent: options.agent ?? new https.Agent({ keepAlive: true }),
+			ua: options.ua ?? `DiscordBot (https://github.com/spec-tacles/rest, ${version})`,
+			mutex: options.mutex ?? new LocalMutex(),
+			retryLimit: options.retryLimit ?? 5,
+		};
+
+		Object.defineProperty(this.options, 'token', { enumerable: false });
+	}
+
+	public post(endpoint: string, body: any, options?: Request) {
+		return this.make({
+			method: 'post',
+			endpoint,
+			body,
+			...options,
+		});
+	}
+
+	public get(endpoint: string, options?: Request) {
+		return this.make({
+			method: 'get',
+			endpoint,
+			...options,
+		});
+	}
+
+	public put(endpoint: string, body: any, options?: Request) {
+		return this.make({
+			method: 'put',
+			endpoint,
+			body,
+			...options,
+		});
+	}
+
+	public delete(endpoint: string, options?: Request) {
+		return this.make({
+			method: 'delete',
+			endpoint,
+			...options,
+		});
+	}
+
+	public patch(endpoint: string, body: any, options?: Request) {
+		return this.make({
+			method: 'patch',
+			endpoint,
+			body,
+			...options,
+		});
+	}
+
+	public make(req: Request): Promise<any> {
+		// configure route and ratelimiter
+		const route = Bucket.makeRoute(req.method ?? 'get', req.endpoint ?? '');
+		let b = this.buckets.get(route);
+		if (!b) {
+			b = new Bucket(this, route);
+			this.buckets.set(route, b);
+		}
+
+		this.format(req);
+		return b.make(req);
+	}
+
+	public format(req: Request): void {
+		if (
+			typeof req.body === 'object' &&
+			!(req.body instanceof ArrayBuffer) &&
+			!(req.body instanceof Readable)
+		) {
+			try {
+				req.body = JSON.stringify(req.body);
+				Rest.setHeader(req, 'Content-Type', 'application/json');
+			} catch {}
+		} else {
+			Rest.setHeader(req, 'Content-Type', 'application/octet-stream');
+		}
+
+		if (req.files) {
+			const form = new FormData();
+			if (Array.isArray(req.files)) for (const f of req.files) form.append(f.name, f.file, f.name);
+			else form.append(req.files.name, req.files.file, req.files.name);
+			if (typeof req.body !== 'undefined') form.append('payload_json', req.body);
+			req.body = form;
+			Rest.setHeader(req, 'Content-Type', form.getHeaders()['content-type']);
+		}
+
+		if (req.reason) Rest.setHeader(req, 'X-Audit-Log-Reason', req.reason);
+
+		if (this.options.token && !Rest.hasHeader(req, 'Authorization')) {
+			Rest.setHeader(req, 'Authorization', `${this.options.tokenType} ${this.options.token}`);
+		}
+
+		Rest.setHeader(req, 'User-Agent', this.options.ua);
+
+		if (!req.agent) req.agent = this.options.agent;
+	}
+
+	public makeURL(endpoint: string): string {
+		return `${this.options.base}/api/v${this.options.version}${endpoint}`;
+	}
+}
